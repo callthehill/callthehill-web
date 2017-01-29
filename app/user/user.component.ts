d72cbf3f0a6854a2ca95cd2cdae04c4967cd4292
Zach Zundel <zach.zundel@utah.edu>
2017-01-29 12:57:39 -0700

    Modified user component

--- app/user/user.component.ts
+++ app/user/user.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+const dummyState: UserState = {
+  logged_in: false,
+  username: ""
+}
+
 @Component({
   selector: 'user',
   templateUrl: 'app/user/user.component.html',
   styleUrls: ['app/user/user.component.css']
 })
 
-export class UserComponent { }
+export class UserComponent { 
+  state = dummyState; 
+}
+
+export class UserState {
+  logged_in: boolean;
+  username: string;
+}
