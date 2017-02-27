070ed81345b6b047e3f681181e9ef3d3284138e2
Zach Zundel <zach.zundel@utah.edu>
2017-02-27 13:01:16 -0700

    Updates to issue

---
+++ app/issue/issue.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'issue',
+  templateUrl: './issue.component.html',
+  styleUrls: ['./issue.component.css']
+})
+export class IssueComponent { }
